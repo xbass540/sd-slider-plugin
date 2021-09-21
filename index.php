d2b47b5396264f1202d177d3de1be85fce3b70f0
Kostas Mavrokefalos <xbass540@yahoo.gr>
2021-09-21 12:53:16 +0300

    1st commit

--- index.php
+++ index.php
@@ -9,6 +9,24 @@
 */
 $plugin_url = plugin_dir_url( __FILE__ );
 
+/** Step 2 (from text above). */
+add_action( 'admin_menu', 'sd_slider_settings' );
+
+/** Step 1. */
+function sd_slider_settings() {
+	add_options_page( 'SD Slider Settings', 'SD Slider', 'manage_options', 'my-unique-identifier', 'sd_plugin_options' );
+}
+
+/** Step 3. */
+function sd_plugin_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>SD Slider Plugin Options</p>';
+	echo '</div>';
+}
+
 wp_enqueue_script('jquery','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
 
 wp_enqueue_script('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js');
